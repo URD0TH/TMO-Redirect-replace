d7f2c953d8558d020ec27bad703c4f9e490928c2
URD0TH <urd0th@gmail.com>
2023-06-08 02:09:23 -0400

    fix code and add domain fitfooders.com

--- Multi-script.meta.js
+++ Multi-script.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name TMO-Redirect-replace
 // @namespace https://github.com/URD0TH/TMO-Redirect-replace
-// @version 2.6.3.2.9
+// @version 2.6.3.2.10
 // @description Quiero Ver Mi MANGA!!!!
 // @author IRhoAias y URD0TH
 // @updateURL https://github.com/URD0TH/TMO-Redirect-replace/raw/main/Multi-script.meta.js
@@ -44,6 +44,7 @@
 // @match https://*.anitirion.com/*
 // @match https://*.paleomotor.com/*
 // @match https://*.animalslegacy.com/*
+// @match https://*.fitfooders.com/*
 // ==/UserScript==
  
 if (window.top !== window.self) //Corre en 2do plano, SOLAMENTWE si tienes el navegador en 1er plano,
@@ -144,6 +145,10 @@ if (currentURL.match("paleomotor.com/news")) {
         
 if (currentURL.match("animalslegacy.com/news")) {
             location.href = location.href.replace("animalslegacy.com/news", "lectortmo.com/viewer");
+        };
+            
+if (currentURL.match("fitfooders.com/news")) {
+            location.href = location.href.replace("fitfooders.com/news", "lectortmo.com/viewer");
         };
         
 // end of script
